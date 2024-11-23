1c65b8a4a344f28fc406d4d65af9fc06e05e3b06
ljraguiar10 <93880318+ljraguiar@users.noreply.github.com>
2024-11-23 13:20:29 -0300

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+const yearSpan = document.getElementById('currentyear');
+yearSpan.textContent = new Date().getFullYear();
+
+const lastModifiedParagraph = document.getElementById('lastModified');
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
